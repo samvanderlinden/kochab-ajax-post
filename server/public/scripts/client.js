5e37a0376e75f9aa409ede8947d4cd6600cdf30d
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-19 12:01:31 -0500

    appended to table and converted cost to US currency

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,8 +10,19 @@ function onReady() {
     })
     .then(function(records) {
         console.log(records);
-        for(let i = 0; i < records.length; i++){
-            $('#recordsList').append(`<li>${records[i].title} by ${records[i].artist}</li>`);
-        }
+        // for(let i = 0; i < records.length; i++){
+        //     $('#recordsList').append(``)
+        //     $('#recordsList').append(`<li>${records[i].title} by ${records[i].artist}</li>`);
+        // }
+        records.forEach(function(record){
+            $('#recordsList').append(
+            `<tr>
+                <td>${record.title}</td>
+                <td>${record.artist}</td>
+                <td>${record.year}</td>
+                <td>${record.cost.toLocaleString('en', { style: 'currency', currency: 'USD' }).slice(0, -3)}</td>
+            </tr>`
+            );
+        })
     })
 }

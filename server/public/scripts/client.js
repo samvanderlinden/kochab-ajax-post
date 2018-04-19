d9a1dc6ce7b855e1a05473a48282953f849a6fd7
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-19 11:28:38 -0500

    records data appended to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,14 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery has been loaded');
+    $.ajax({
+        type: 'GET',
+        url: '/records'
+    })
+    .then(function(records) {
+        console.log(records);
+        for(let i = 0; i < records.length; i++){
+            $('#recordsList').append(`<li>${records[i].title} by ${records[i].artist}</li>`);
+        }
+    })
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,16 @@ const express=require('express');
 const app=express(); // this creates an instance of the express server
 const PORT=5000;
 
+// this will live in database shortly
+const recordCollection=require('./modules/record-collection');
+
 app.use(express.static('server/public'));
+
+// routes (get, post, put delete requests)
+app.get('/records', function(req, res){
+    console.log(recordCollection);
+    res.send(recordCollection); 
+});
 
 app.listen(PORT, () => {
     console.log(`up and running on port: ${PORT}`);
